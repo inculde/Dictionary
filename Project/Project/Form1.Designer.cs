813d82d8b0ca89dc31b5f8f1e1c5cd21fca0d3e8
DeesLeaFEX4MW <dees.lea@freemail.hu>
2016-10-15 22:46:26 +0200

    Szoszedet form

--- Project/Project/Form1.Designer.cs
+++ Project/Project/Form1.Designer.cs
@@ -53,7 +53,8 @@
             this.tanulásiSegédletToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(482, 28);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(362, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -64,28 +65,28 @@
             this.bezárásToolStripMenuItem,
             this.kilépésToolStripMenuItem});
             this.fájlToolStripMenuItem.Name = "fájlToolStripMenuItem";
-            this.fájlToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fájlToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fájlToolStripMenuItem.Text = "Fájl";
             this.fájlToolStripMenuItem.Click += new System.EventHandler(this.fájlToolStripMenuItem_Click);
             // 
             // megnyitásToolStripMenuItem
             // 
             this.megnyitásToolStripMenuItem.Name = "megnyitásToolStripMenuItem";
-            this.megnyitásToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.megnyitásToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
             this.megnyitásToolStripMenuItem.Text = "Megnyitás";
             this.megnyitásToolStripMenuItem.Click += new System.EventHandler(this.megnyitásToolStripMenuItem_Click);
             // 
             // bezárásToolStripMenuItem
             // 
             this.bezárásToolStripMenuItem.Name = "bezárásToolStripMenuItem";
-            this.bezárásToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.bezárásToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
             this.bezárásToolStripMenuItem.Text = "Bezárás";
             this.bezárásToolStripMenuItem.Click += new System.EventHandler(this.bezárásToolStripMenuItem_Click);
             // 
             // kilépésToolStripMenuItem
             // 
             this.kilépésToolStripMenuItem.Name = "kilépésToolStripMenuItem";
-            this.kilépésToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.kilépésToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
             this.kilépésToolStripMenuItem.Text = "Kilépés";
             this.kilépésToolStripMenuItem.Click += new System.EventHandler(this.kilépésToolStripMenuItem_Click);
             // 
@@ -95,20 +96,20 @@
             this.szótárToolStripMenuItem,
             this.megjelenítésToolStripMenuItem});
             this.beállításokToolStripMenuItem.Name = "beállításokToolStripMenuItem";
-            this.beállításokToolStripMenuItem.Size = new System.Drawing.Size(93, 24);
+            this.beállításokToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
             this.beállításokToolStripMenuItem.Text = "Beállítások";
             // 
             // szótárToolStripMenuItem
             // 
             this.szótárToolStripMenuItem.Name = "szótárToolStripMenuItem";
-            this.szótárToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.szótárToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
             this.szótárToolStripMenuItem.Text = "Szótár";
             this.szótárToolStripMenuItem.Click += new System.EventHandler(this.szótárToolStripMenuItem_Click);
             // 
             // megjelenítésToolStripMenuItem
             // 
             this.megjelenítésToolStripMenuItem.Name = "megjelenítésToolStripMenuItem";
-            this.megjelenítésToolStripMenuItem.Size = new System.Drawing.Size(169, 26);
+            this.megjelenítésToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
             this.megjelenítésToolStripMenuItem.Text = "Megjelenítés";
             // 
             // tanulásiSegédletToolStripMenuItem
@@ -118,45 +119,48 @@
             this.tesztToolStripMenuItem,
             this.statisztikaToolStripMenuItem});
             this.tanulásiSegédletToolStripMenuItem.Name = "tanulásiSegédletToolStripMenuItem";
-            this.tanulásiSegédletToolStripMenuItem.Size = new System.Drawing.Size(136, 24);
+            this.tanulásiSegédletToolStripMenuItem.Size = new System.Drawing.Size(110, 20);
             this.tanulásiSegédletToolStripMenuItem.Text = "Tanulási segédlet";
             this.tanulásiSegédletToolStripMenuItem.Click += new System.EventHandler(this.tanulásiSegédletToolStripMenuItem_Click);
             // 
             // szótárfüzetToolStripMenuItem
             // 
             this.szótárfüzetToolStripMenuItem.Name = "szótárfüzetToolStripMenuItem";
-            this.szótárfüzetToolStripMenuItem.Size = new System.Drawing.Size(159, 26);
+            this.szótárfüzetToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.szótárfüzetToolStripMenuItem.Text = "Szótárfüzet";
+            this.szótárfüzetToolStripMenuItem.Click += new System.EventHandler(this.szótárfüzetToolStripMenuItem_Click);
             // 
             // tesztToolStripMenuItem
             // 
             this.tesztToolStripMenuItem.Name = "tesztToolStripMenuItem";
-            this.tesztToolStripMenuItem.Size = new System.Drawing.Size(159, 26);
+            this.tesztToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.tesztToolStripMenuItem.Text = "Teszt";
             // 
             // statisztikaToolStripMenuItem
             // 
             this.statisztikaToolStripMenuItem.Name = "statisztikaToolStripMenuItem";
-            this.statisztikaToolStripMenuItem.Size = new System.Drawing.Size(159, 26);
+            this.statisztikaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.statisztikaToolStripMenuItem.Text = "Statisztika";
             // 
             // szovegTxt
             // 
-            this.szovegTxt.Location = new System.Drawing.Point(12, 31);
+            this.szovegTxt.Location = new System.Drawing.Point(9, 25);
+            this.szovegTxt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.szovegTxt.Multiline = true;
             this.szovegTxt.Name = "szovegTxt";
             this.szovegTxt.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.szovegTxt.Size = new System.Drawing.Size(439, 392);
+            this.szovegTxt.Size = new System.Drawing.Size(330, 319);
             this.szovegTxt.TabIndex = 1;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(482, 453);
+            this.ClientSize = new System.Drawing.Size(362, 368);
             this.Controls.Add(this.szovegTxt);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = " ";
             this.menuStrip1.ResumeLayout(false);
---
+++ Project/Project/Form3.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void vocabularyBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.vocabularyBindingSource.EndEdit();
+            //this.tableAdapterManager.UpdateAll(this.dbDataSet);
+
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dbDataSet.Vocabulary' table. You can move, or remove it, as needed.
+            this.vocabularyTableAdapter.Fill(this.dbDataSet.Vocabulary);
+
+        }
+
+        private void bindingNavigatorDeleteItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.vocabularyBindingSource.EndEdit();
+            
+        }
+
+        
+
+        
+        
+    }
+}
