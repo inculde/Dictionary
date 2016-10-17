1beac7d4701e19959a6ffd639da755e2850ebf7c
Barta Éva Anna <bartaeva89@gmail.com>
2016-10-17 09:41:42 +0200

    Bemutatora tartalommal

---
+++ Bemutato/Bemutato/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Bemutato
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.szovegTxt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // szovegTxt
+            // 
+            this.szovegTxt.Location = new System.Drawing.Point(0, 0);
+            this.szovegTxt.Multiline = true;
+            this.szovegTxt.Name = "szovegTxt";
+            this.szovegTxt.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.szovegTxt.Size = new System.Drawing.Size(282, 191);
+            this.szovegTxt.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(101, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Megnyitás";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.szovegTxt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox szovegTxt;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
